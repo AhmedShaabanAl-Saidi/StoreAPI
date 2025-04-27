e01feffcd3d7482f4ea037098c4f5468b5635c76
AhmedShaabanAl-Saidi <ahmedshaaban123321@gmail.com>
2025-04-27 11:05:24 +0300

    Exception Middleware

---
+++ Store.API/Middlewares/GlobalErrorHandlingMiddleware.cs
@@ -0,0 +1,43 @@
+﻿using System.Net;
+using Shared.ErrorModels;
+
+namespace Store.API.Middlewares
+{
+    public class GlobalErrorHandlingMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<GlobalErrorHandlingMiddleware> _logger;
+
+        public GlobalErrorHandlingMiddleware(RequestDelegate next,ILogger<GlobalErrorHandlingMiddleware> logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+
+        public async Task InvokeAsync(HttpContext httpContext)
+        {
+            try
+            {
+                await _next(httpContext);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Something Went Wrong {ex}");
+                await HandleExeption(httpContext, ex);
+            }
+        }
+
+        private async Task HandleExeption(HttpContext httpContext,Exception ex)
+        {
+            httpContext.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+            httpContext.Response.ContentType = "application/json";
+            var response = new ErrorDetails
+            {
+                ErrorMessage = ex.Message,
+            };
+
+            response.StatusCode = httpContext.Response.StatusCode;
+            await httpContext.Response.WriteAsync(response.ToString());
+        }
+    }
+}
