5eca8fb26c2e1410b5649a699eab98fa8d959f99
AhmedShaabanAl-Saidi <ahmedshaaban123321@gmail.com>
2025-04-29 09:59:45 +0300

    Authentication Controller

---
+++ Presentation/Controllers/AuthenticationController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Services.Abstractions;
+using Shared.IdentityDtos;
+
+namespace Presentation.Controllers
+{
+    public class AuthenticationController(IServiceManager serviceManager) : ApiController
+    {
+        [HttpPost("Login")]
+        public async Task<ActionResult<UserResultDto>> Login(LoginDto loginDto)
+            => Ok(await serviceManager.AuthenticationService.LoginAsync(loginDto));
+
+        [HttpPost("Register")]
+        public async Task<ActionResult<UserResultDto>> Register(RegisterDto registerDto)
+            => Ok(await serviceManager.AuthenticationService.RegisterAsync(registerDto));
+    }
+}
