8adc5ed2b820ec47b4839617cd4e198a4afcc8f3
AhmedShaabanAl-Saidi <ahmedshaaban123321@gmail.com>
2025-05-04 10:46:16 +0300

    Add Authorization To Swagger

--- Store.API/Extensions/PresentationServicesExtension.cs
+++ Store.API/Extensions/PresentationServicesExtension.cs
@@ -1,5 +1,6 @@
 ﻿using System.Text.Json.Serialization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.OpenApi.Models;
 using Store.API.Factories;
 
 namespace Store.API.Extensions
@@ -18,9 +19,49 @@ namespace Store.API.Extensions
                 options.InvalidModelStateResponseFactory = ApiResponseFactory.CustomValidtionErrorRespose;
             });
 
+
+            services.ConfigureSwagger();
+            return services;
+        }
+
+        public static IServiceCollection ConfigureSwagger(this IServiceCollection services)
+        {
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             services.AddEndpointsApiExplorer();
-            services.AddSwaggerGen();
+            services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "Store API",
+                    Version = "v1",
+                    Description = "Store API Documentation"
+                });
+
+                options.AddSecurityDefinition(name: "Bearer", securityScheme: new OpenApiSecurityScheme
+                {
+                    In = ParameterLocation.Header,
+                    Description = "Enter Bearer [space] and then your token",
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer",
+                    BearerFormat = "JWT"
+                });
+
+                options.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            }
+                        },
+                        new string[] {}
+                    }
+                });
+            });
 
             return services;
         }
